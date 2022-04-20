a7671914019307677ef59aef9d0f59561eab61d7
plx-edu <plx-edu@hotmail.com>
2022-04-20 14:41:38 +0200

    installed  cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const port = 3005;
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello" });
