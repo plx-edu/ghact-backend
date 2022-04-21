2589d9bf6d3635985040a0628a7b08f37de08e32
plx-edu <plx-edu@hotmail.com>
2022-04-21 10:36:18 +0200

    uncommented pm2 command for safety, added /api to path

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const port = 3005;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ msg: "Hello" });
 });
 
-app.get("/hello", (req, res) => {
+app.get("/api/hello", (req, res) => {
   res.json({ msg: "Greetings & Salutations", status: 200 });
 });
 
-app.get("/dashboard", (req, res) => {
+app.get("/api/dashboard", (req, res) => {
   res.json({ msg: "Dashing when I'm bored", status: 201 });
 });
 
